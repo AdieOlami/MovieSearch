ab4deb4642bd5864527693330c2488218fccbd99
Adie Olalekan <ugbeadie@gmail.com>
2019-03-07 10:30:21 +0100

    Initial Commit

---
+++ OneFi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneFi
+//
+//  Created by Olar's Mac on 3/7/19.
+//  Copyright © 2019 Adie Olami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
